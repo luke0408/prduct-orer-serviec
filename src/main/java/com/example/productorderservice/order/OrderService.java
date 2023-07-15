35e7be6f5faa95e1262d4c826c89215a07d5a64e
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2023-07-15 14:44:50 +0900

    ♻️ [Refactor]: spring boot test to api test (#4)

--- src/main/java/com/example/productorderservice/order/OrderService.java
+++ src/main/java/com/example/productorderservice/order/OrderService.java
@@ -1,8 +1,16 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/orders")
 @Component
 class OrderService {
     private final OrderPort orderPort;
@@ -11,11 +19,13 @@ class OrderService {
         this.orderPort = orderPort;
     }
 
-    public void createOrder(OrderServiceRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> createOrder(@RequestBody OrderServiceRequest request) {
         final Product product = orderPort.getProductById(request.productId());
 
         final Order order = new Order(product, request.quantity());
 
         orderPort.save(order);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
